229cbfc9a2aa97518ec5c3083de9d235c2daeacd
6ALLIKA <skorik009@gmail.com>
2020-05-23 20:12:01 +0300

    full cleanup

--- src/main/java/ma/internetshop/security/AuthenticationService.java
+++
@@ -1,8 +0,0 @@
-package ma.internetshop.security;
-
-import ma.internetshop.exceptions.AuthenticationException;
-import ma.internetshop.model.User;
-
-public interface AuthenticationService {
-    User login(String login, String password) throws AuthenticationException;
-}
--- src/main/java/ma/internetshop/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package ma.internetshop.service;
-
-import java.util.Optional;
-import ma.internetshop.model.User;
-
-public interface UserService extends GenericService<User, Long> {
-
-    Optional<User> getByLogin(String login);
-}
